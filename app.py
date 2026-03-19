8ee9a4a3ad6bb0a6b8cf8d1f312c1d49c5709ae0
finleydev <finleygersdorff@gmail.com>
2026-03-19 20:49:12 +0100

    Add entry in browser

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from datetime import datetime
 import csv
 
-app = Flask("__name__")
+app = Flask(__name__)
 
 def load_entries():
     entries = []
@@ -17,10 +18,25 @@ def load_entries():
 
 
 @app.route("/")
+@app.route("/home")
 def home():
     entries = load_entries()
     total = sum(int(e["amount"])for e in entries)
     return render_template("index.html", entries=entries, total=total)
+
+@app.route("/add", methods=["POST"])
+def add():
+    category = request.form["category"]
+    amount = request.form["amount"]
+    date = datetime.now().strftime("%Y-%m%d %H:%M")
+
+    with open("tracker.csv", "a", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=["category", "amount", "date"])
+        writer.writerow({"category": category, "amount": amount, "date": date})
+
+    return redirect("/home")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
